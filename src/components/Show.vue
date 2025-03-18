ef4b0157056c9429b0dd549b4f9219cd53a547e5
Sourav Roy <souravkumarroy813965@gmail.com>
2025-03-18 12:02:22 +0600

    forward and back button are fixed

--- src/components/Show.vue
+++ src/components/Show.vue
@@ -10,7 +10,7 @@ import { RouterLink } from 'vue-router';
 import Card from './Card.vue';
 import Nav from './nav.vue';
 import axios from 'axios';
-import { ref, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 const store = useStore();
 let res, tempArr;
@@ -18,57 +18,78 @@ const obj = ref({});
 const currentPage = ref(0);
 const route = useRoute();
 const footerOption = ref([]);
+const curr = ref(1);
+const total = ref(0);
 
 const fetchApi = async () => {
     const api = await axios.get(`https://hacker-news.firebaseio.com/v0/${route.params.stories}.json`);
     res = api.data;
 };
 
+watch(route , ()=>{
+    console.log('dukse');
+    curr.value = 1;
+});
 watch([currentPage, route], async () => {
+    console.log('eta teo dukse');
     await fetchApi();
+    total.value = Math.ceil(res.length / 25);
     tempArr = res.slice(currentPage.value, currentPage.value + 25);
     footerOption.value = [];
     tempArr.forEach(async (id) => {
         obj.value = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
-        store.commit('timeCal' , obj.value.data.time);
+        store.commit('timeCal', obj.value.data.time);
         footerOption.value.push({
             title: `${obj.value.data.title}`,
             score: `${obj.value.data.score}`,
             by: `by ${obj.value.data.by}`,
-            comments: obj.value.data.descendants !== undefined &&  obj.value.data.descendants !== 0? `${obj.value.data.descendants} Comments` : ``,
+            comments: obj.value.data.descendants !== undefined && obj.value.data.descendants !== 0 ? `${obj.value.data.descendants} Comments` : ``,
             created: `Created ${store.state.time} ${store.state.when} ago`,
             slash: `|`,
             url: obj.value.data.url,
-            kids : obj.value.data.kids === undefined ? [] : obj.value.data.kids,
+            kids: obj.value.data.kids === undefined ? [] : obj.value.data.kids,
         });
     });
-    store.commit('setopt' , footerOption);
+    store.commit('setopt', footerOption);
 }, { immediate: true });
 
 function backPage() {
     if (currentPage.value !== 0) {
         currentPage.value -= 25;
+        curr.value = Math.floor(currentPage.value / 25) + 1;
+        console.log('back currpage ' , currentPage.value);
+        console.log('back currcnt ' , curr.value);
     }
 }
 
 function forwarPage() {
     if (currentPage.value < res.length) {
         currentPage.value += 25;
+        curr.value = Math.floor(currentPage.value / 25) + 1;
     }
 }
 </script>
 
 <template>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <Nav />
     <br>
-    <main>
-        <Card/>
-        <button @click="backPage"> Back </button>
-        <button @click="forwarPage"> Forward </button>
+    <main class="par">
+        <div>
+            <Card />
+        </div>
+        <div class="btn">
+            <button @click="backPage"> <i class="fa fa-arrow-circle-left" style="font-size:35px;color:red"></i>
+            </button>
+            <p> {{ curr }} of {{ total }}</p>
+            <button @click="forwarPage"> <i class="fa fa-arrow-circle-right" style="font-size:35px;color:red"></i>
+            </button>
+        </div>
     </main>
 </template>
 
 <style>
+
 .card {
     display: flex;
     min-height: 100px;
@@ -79,13 +100,15 @@ function forwarPage() {
     flex-direction: column;
     justify-content: space-evenly;
 }
-.titleBar{
+
+.titleBar {
     font-size: 20px;
-    color : #4D606D;
+    color: #4D606D;
     font-weight: 400;
 }
+
 .footerOpt {
-    width: 60%;
+    width: 68%;
     display: flex;
     flex-direction: row;
     justify-content: space-around;
@@ -119,4 +142,13 @@ a:active {
 .card:hover {
     box-shadow: 2px 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
+
+.btn {
+    display: flex;
+    justify-content: center; 
+    align-items: center; 
+    margin-top: 20px; 
+    gap: 10px; 
+}
+
 </style>
